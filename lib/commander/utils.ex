318e2ba00dda6dbf41f9965559de96ae24a4dad4
Juan <20613820+PurgePJ@users.noreply.github.com>
2018-08-03 20:26:16 +0200

    Added priority speaker

--- lib/commander/utils.ex
+++ lib/commander/utils.ex
@@ -13,6 +13,7 @@ defmodule Coxir.Commander.Utils do
     MANAGE_GUILD: 1 <<< 5,
     ADD_REACTIONS: 1 <<< 6,
     VIEW_AUDIT_LOG: 1 <<< 7,
+    PRIORITY_SPEAKER: 1 <<< 8,
 
     VIEW_CHANNEL: 1 <<< 10,
     SEND_MESSAGES: 1 <<< 11,
